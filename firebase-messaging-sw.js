9f0bde9ad3a0a163ffc7ce2c3275835fc69a1b48
bechir <beshir.chedly@gmail.com>
2021-04-01 12:00:20 +0100

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,28 +2,25 @@ importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
 
 // if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('../firebase-messaging-sw.js')
-//     .then(function (registration) {
-//       console.log('Registration successful, scope is:', registration.scope);
-//     }).catch(function (err) {
-//       console.log('Service worker registration failed, error:', err);
-//     });
-// }
-var firebaseConfig = {
-  apiKey: "AIzaSyBdaRtJEHWOnqNeV_2UXkWHCxwsTUrsz74",
-  authDomain: "my-pwa-985de.firebaseapp.com",
-  projectId: "my-pwa-985de",
-  storageBucket: "my-pwa-985de.appspot.com",
+//     navigator.serviceWorker.register('../firebase-messaging-sw.js')
+//       .then(function(registration) {
+//         console.log('Registration successful, scope is:', registration.scope);
+//       }).catch(function(err) {
+//         console.log('Service worker registration failed, error:', err);
+//       });
+//     }
+
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('../firebase-messaging-sw.js')
+      .then(function(registration) {
+        console.log('Registration successful, scope is:', registration.scope);
+      }).catch(function(err) {
+        console.log('Service worker registration failed, error:', err);
+      });
+    }
+
+firebase.initializeApp({
   messagingSenderId: "263147757383",
-  appId: "1:263147757383:web:c66beecfe512d39f6df662"
-};
-firebase.initializeApp(firebaseConfig);
+  })
 
-
-
-
-// firebase.initializeApp({
-//   messagingSenderId: "263147757383",
-// })
-
-const initMessaging = firebase.messaging();
+const initMessaging = firebase.messaging()
