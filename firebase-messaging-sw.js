1cc9bfa8cad2909106675340f1661906063af2a5
bechir <beshir.chedly@gmail.com>
2021-04-02 09:09:58 +0100

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,10 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
 
 console.warn("firebase work !!  : )")
 
-    
-
 firebase.initializeApp({
-  messagingSenderId: "263147757383",
-  })
+  messagingSenderId: "263147757383"
+});
 
 const initMessaging = firebase.messaging()
+
+
+
+
+
